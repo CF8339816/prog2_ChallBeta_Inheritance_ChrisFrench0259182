0b6d8af46c3ec51ed6c68eb89c98ad23e991867d
CF8339816 <chris_french_3dga@hotmail.com>
2026-01-16 12:17:38 -0400

    set up rando select for attacker and defender arctypes for head to head

--- prog2_ChallBeta_Inheritance_ChrisFrench0259182/Program.cs
+++ prog2_ChallBeta_Inheritance_ChrisFrench0259182/Program.cs
@@ -7,9 +7,53 @@ using System.Threading.Tasks;
 namespace prog2_ChallBeta_Inheritance_ChrisFrench0259182
 {
     internal class Program
-    {
+    {       
+        
+            //static Random combatant1 = new Random(1, 4);
+            //static Random combatant2 = new Random(1, 4);
+        
+        
         static void Main(string[] args)
         {
+            Character player1 = new Archer("PewPew Pointystick");
+            Character player2 = new Orc("Smashie McStabface");
+            Character player3 = new Wizard("Bearddress Zappyfingers");
+
+
+             Random combatant1 = new Random(1, 4);
+             Random combatant2 = new Random(1, 4);
+
+            if (combatant1 =  1)
+            {
+                Character attacker = player1;
+            }
+
+            if (combatant1 = 2)
+            {
+                Character attacker = player2;
+            }
+
+            if (combatant1 = 3)
+            {
+                Character attacker = player3;
+            }
+
+            if (combatant2 = 1)
+            {
+                Character defender = player1;
+            }
+
+            if (combatant2 = 2)
+            {
+                Character defender = player2;
+            }
+
+            if (combatant2 = 3)
+            {
+                Character defender = player3;
+            }
+
+
 
         }
     }
@@ -24,7 +68,7 @@ namespace prog2_ChallBeta_Inheritance_ChrisFrench0259182
         public int _minAttack { get; set; }
         public int _maxAttack { get; set; }
 
-        protected Random rando = new Random();
+        protected Random rando = new Random(); //allows rando to be accessed by inherators of the class character
        
      public Character(string name, int health, int speed, int minAtk, int maxAtk)
         {
