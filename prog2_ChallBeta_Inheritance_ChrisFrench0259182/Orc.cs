41b9e3ca9835dbfbe76313bafeece29d61b715e9
CF8339816 <chris_french_3dga@hotmail.com>
2026-01-15 23:59:23 -0400

    Started character and set up Class tabs for Orc Archer and Wizard

---
+++ prog2_ChallBeta_Inheritance_ChrisFrench0259182/Orc.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prog2_ChallBeta_Inheritance_ChrisFrench0259182
+{
+    internal class Orc
+    {
+
+
+
+
+    }
+}
--- prog2_ChallBeta_Inheritance_ChrisFrench0259182/Program.cs
+++ prog2_ChallBeta_Inheritance_ChrisFrench0259182/Program.cs
@@ -10,6 +10,32 @@ namespace prog2_ChallBeta_Inheritance_ChrisFrench0259182
     {
         static void Main(string[] args)
         {
+
         }
+    }
+
+  
+
+    public class Character
+    {
+        public string _name { get; set; }
+        public int _health { get; set; }
+        public int _speed { get; set; }
+        public int _minAttack { get; set; }
+        public int _maxAttack { get; set; }
+
+        protected Random rando = new Random();
+       
+     public Character(string name, int health, int speed, int minAtk, int maxAtk)
+        {
+            _name = name;
+            _health = health;
+            _speed = speed;
+            _minAttack = minAtk;
+            _maxAttack = maxAtk;
+        }
+
+
+  
     }
 }
