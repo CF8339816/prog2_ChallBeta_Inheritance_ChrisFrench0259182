490dcc3bed1a426e9b38df979101619e4d5fa2da
CF8339816 <chris_french_3dga@hotmail.com>
2026-01-18 17:59:51 -0400

    could not get the extewrnal classes to reference corrrectly  trying with character classes in main program

--- prog2_ChallBeta_Inheritance_ChrisFrench0259182/Program.cs
+++ prog2_ChallBeta_Inheritance_ChrisFrench0259182/Program.cs
@@ -181,4 +181,106 @@ namespace prog2_ChallBeta_Inheritance_ChrisFrench0259182
             Console.ReadKey(true);
         }
     }
+    class Orc : Character
+    {
+        public Orc(string name) : base(name, 150, 5, 10, 25)
+        {
+            // Console.WriteLine($"\n Orc attacks maybe");
+        }
+
+        public override void Attack(Character target)
+        {
+            base.Attack(target);
+
+            int heal = 10; //Thrill of combat adrenaline healing
+            _health += heal;
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine($"Adreanaline, rage, and the thrill of battle invigorates {_name}, they heal for {heal}!");
+            Console.ReadKey(true);
+        }
+
+
+
+    }
+
+    class Archer : Character
+
+    {
+        public Archer(string name) : base(name, 120, 18, 8, 15)
+        {
+
+        }
+        public override void Attack(Character target)
+        {
+            base.Attack(target);
+
+            if (rando.Next(0, 100) < 30) // chance  for rapid shot 
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine($"{_name} is as quick as an oil covered snake shot from a cannon...and attacks a second time!");
+                base.Attack(target);
+                Console.ReadKey(true);
+            }
+        }
+
+
+    }
+
+
+    class Wizard : Character
+
+    {
+        public Wizard(string name) : base(name, 75, 8, 15, 40)
+        {
+            // Console.WriteLine($"\n wizard attacks maybe");
+        }
+
+
+        public override void TakeDamage(int damage)// cool ability spell:damage reflect, reflects damage back to attacker
+        {
+            //int reflect = damage;
+            if (rando.Next(0, 100) < 20)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine($"{_name} casts a spell to asorbe, empower and reflect Damage  back to attacker");
+                int reducedDamage = 0;
+                //base.TakeDamage(reducedDamage);
+                Console.ReadKey(true);
+            }
+            //Reflect(Character target);
+
+        }
+
+        //public void Reflect(Character target)
+        //{
+        //    Console.Write("Spellblast attack! ");
+        //    base.Attack(target);
+        //    base.Attack(target);
+        //    base.Attack(target);
+
+
+        //    //target._health-damage;
+        //}
+
+
+        public override void Attack(Character target)
+        {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.Write("Spellblast attack! ");
+            base.Attack(target);
+            Console.ReadKey(true);
+
+            //target._health-damage;
+        }
+
+
+
+    }
+
+
+
+
+
+
+
 }
